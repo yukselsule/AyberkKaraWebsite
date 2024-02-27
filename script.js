1079ba7031a06c536f00d176fc6938f554e28c43
yukselsule <sulecantekin@gmail.com>
2024-02-27 20:06:37 +0300

    done before logo

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+// const sectionHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  { root: null, threshold: 0, rootMargin: "-67px" }
-);
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   { root: null, threshold: 0, rootMargin: "-67px" }
+// );
 
-obs.observe(sectionHeroEl);
+// obs.observe(sectionHeroEl);
 
 // Mobile nav
 
