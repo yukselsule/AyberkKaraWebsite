d4ef3e69d4510a5d02fc0120a18fed1a6a315efe
yukselsule <sulecantekin@gmail.com>
2024-03-19 17:43:18 +0300

    revealing sections

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 "use strict";
+// reveal sections
+const allSections = document.querySelectorAll(".section");
+const revealSections = function (entries, observer) {
+  const [entry] = entries;
 
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
 // Mobile nav
 
 const menuBtn = document.querySelector(".menu");
